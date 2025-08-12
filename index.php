767e6cda6498de5c5134baecf40516a21c7e29f4
Tung Pham <tung.42@gmail.com>
2025-08-12 18:02:36 +0700

    possible fixes

--- index.php
+++ index.php
@@ -1,28 +1,23 @@
 <?php get_header(); ?>
 <main>
     <?php
-    if (did_action('elementor/loaded') && \Elementor\Plugin::$instance->preview->is_preview_mode()) {
-        echo \Elementor\Plugin::$instance->frontend->get_builder_content_for_display(get_queried_object_id());
-    } else {
-        if (have_posts()) :
-            ?>
-            <div class="posts-grid">
-                <?php while (have_posts()) : the_post(); ?>
-                    <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-                        <header class="entry-header">
-                            <h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-                        </header>
-                        <div class="entry-content">
-                            <?php the_content(); ?>
-                        </div>
-                    </article>
-                <?php endwhile; ?>
-            </div>
-            <?php the_posts_pagination(); ?>
-        <?php else : ?>
-            <p><?php _e('No posts found.', 'tungtheme'); ?></p>
-        <?php endif;
-    }
-    ?>
+    if (have_posts()) :
+        ?>
+        <div class="posts-grid">
+            <?php while (have_posts()) : the_post(); ?>
+                <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+                    <header class="entry-header">
+                        <h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+                    </header>
+                    <div class="entry-content">
+                        <?php the_content(); ?>
+                    </div>
+                </article>
+            <?php endwhile; ?>
+        </div>
+        <?php the_posts_pagination(); ?>
+    <?php else : ?>
+        <p><?php _e('No posts found.', 'tungtheme'); ?></p>
+    <?php endif; ?>
 </main>
 <?php get_footer(); ?>
--- single.php
+++ single.php
@@ -3,20 +3,16 @@
     <?php
     while (have_posts()) :
         the_post();
-        if (did_action('elementor/loaded') && (\Elementor\Plugin::$instance->preview->is_preview_mode() || \Elementor\Plugin::$instance->documents->get(get_the_ID())->is_built_with_elementor())) {
-            echo \Elementor\Plugin::$instance->frontend->get_builder_content_for_display(get_the_ID());
-        } else {
-            ?>
-            <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-                <header class="entry-header">
-                    <h1 class="entry-title"><?php the_title(); ?></h1>
-                </header>
-                <div class="entry-content">
-                    <?php the_content(); ?>
-                </div>
-            </article>
-            <?php
-        }
+        ?>
+        <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+            <header class="entry-header">
+                <h1 class="entry-title"><?php the_title(); ?></h1>
+            </header>
+            <div class="entry-content">
+                <?php the_content(); ?>
+            </div>
+        </article>
+        <?php
     endwhile;
     ?>
 </main>
