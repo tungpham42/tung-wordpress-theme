eba59bf045d06db01e5c60d5a353e01d185095f3
Tung Pham <tung.42@gmail.com>
2025-08-12 18:30:28 +0700

    style again

--- index.php
+++ index.php
@@ -6,9 +6,7 @@
         <div class="posts-grid">
             <?php while (have_posts()) : the_post(); ?>
                 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-                    <header class="entry-header">
-                        <h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-                    </header>
+                    <h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
                     <div class="entry-content">
                         <?php the_content(); ?>
                     </div>
--- page.php
+++ page.php
@@ -5,9 +5,7 @@
         the_post();
         ?>
         <article id="page-<?php the_ID(); ?>" <?php post_class(); ?>>
-            <header class="entry-header">
-                <h1 class="entry-title"><?php the_title(); ?></h1>
-            </header>
+            <h1 class="entry-title"><?php the_title(); ?></h1>
             <div class="entry-content">
                 <?php the_content(); ?>
             </div>
