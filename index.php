096ac7741ddf34799fbef0b1afba721e4ff03404
Tung Pham <tung.42@gmail.com>
2025-08-13 15:45:26 +0700

    entry-header

--- index.php
+++ index.php
@@ -6,7 +6,9 @@
         <div class="posts-grid">
             <?php while (have_posts()) : the_post(); ?>
                 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-                    <h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+                    <header class="entry-header">
+                        <h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+                    </header>
                     <div class="entry-content">
                         <?php the_content(); ?>
                     </div>
--- single.php
+++ single.php
@@ -5,7 +5,9 @@
         the_post();
         ?>
         <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-            <h1 class="entry-title"><?php the_title(); ?></h1>
+            <header class="entry-header">
+                <h1 class="entry-title"><?php the_title(); ?></h1>
+            </header>
             <div class="entry-content">
                 <?php the_content(); ?>
             </div>
